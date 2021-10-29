6390e1e75967021d2dc9ebc86aba83ac939bdcbb
Takeshi2997 <takeshi6.6260@gmail.com>
2021-10-29 14:02:39 +0900

    test ver

--- hamiltonian.jl
+++ hamiltonian.jl
@@ -1,30 +1,37 @@
-function hamiltonian(i::Integer, x::State, y::T, model::GPmodel) where {T<:Complex}
-    hamiltonian_ising(i, x, y, model)
-#    hamiltonian_heisenberg(x, y, model)
-#    hamiltonian_XY(x, y, model)
+using SparseArrays
+
+I = sparse([1.0+0.0im 0.0; 0.0 1.0+0.0im])
+S¹ = sparse([0.0 1.0+0.0im; 1.0+0.0im 0.0]) / 2.0
+S² = sparse([0.0 -1.0im; 1.0im 0.0]) / 2.0
+S³ = sparse([1.0+0.0im 0.0; 0.0 -1.0+0.0im]) / 2.0
+
+function ⊗(A,B,C...)
+    A = kron(A, B)
+    for Ci in C
+        A = kron(A, Ci)
+    end
+    return A
 end
 
-function hamiltonian_heisenberg(i::Integer, x::State, y::T, model::GPmodel) where {T<:Complex}
-    xflip_spin = copy(x.spin)
-    xflip_spin[i] *= -1
-    xflip = State(xflip_spin)
-    yflip = predict(xflip, model)
-    -c.J * (1.0 + (2.0 * exp(yflip - y) - 3.0) * (x.spin[i] * x.spin[i%c.NSpin+1] < 0)) / 4.0
+function set_spins(N, sites, σs)
+    list_mats = fill(I, N)
+    for (site, σ) in zip(sites, σs)
+        list_mats[site] = σ
+    end
+    return list_mats
 end
 
-function hamiltonian_ising(i::Integer, x::State, y::T, model::GPmodel) where {T<:Complex}
-    xflip_spin = copy(x.spin)
-    xflip_spin[i] *= -1
-    xflip = State(xflip_spin)
-    yflip = predict(xflip, model)
-    -x.spin[i] * x.spin[i%c.NSpin+1] / 4.0 - c.H * exp(yflip - y) / 2.0
+function hamiltonian_ising(N::T) where {T<:Integer}
+    H = ⊗(set_spins(N, [N,1], [S³, S³])...)
+    for i in 1:N-1
+        H += ⊗(set_spins(N, [i,i+1], [S³,S³])...)
+        H += -c.H * ⊗(set_spins(N, [i], [S¹]))
+    end
+    return H
 end
 
-function hamiltonian_XY(i::Integer, x::State, y::T, model::GPmodel) where {T<:Complex}
-    xflip_spin = copy(x.spin)
-    xflip_spin[i] *= -1
-    xflip = State(xflip_spin)
-    yflip = predict(xflip, model)
-    c.t * exp(yflip - y) * (x.spin[i] * x.spin[i%c.NSpin+1] < 0)
+function hamiltonian(N::T) where {T<:Integer}
+    hamiltonian_ising(N)
+#    hamiltonian_heisenberg(Ω)
+#    hamiltonian_XY(Ω)
 end
-
--- setup.jl
+++ setup.jl
@@ -3,26 +3,22 @@ struct GP_Data{T<:Real, S<:Integer}
     NData::S
     NMC::S
     MCSkip::S
-    iT::S
+    NUM::S
     H::T
     t::T
     J::T
     l::T
-    B::T
-    λ::T
 end
 function GP_Data()
-    NSpin = 80
+    NSpin = 16
     NData = 64
     NMC = 1024
     MCSkip = 16
-    iT = 100
+    NUM = 100
     H = 2.0
     t = 1.0
     J = 1.0
     l = 0.8
-    B = 1.0
-    λ = 0.2
-    GP_Data(NSpin, NData, NMC, MCSkip, iT, H, t, J, l, B, λ)
+    GP_Data(NSpin, NData, NMC, MCSkip, NUM, H, t, J, l)
 end
 c = GP_Data()
